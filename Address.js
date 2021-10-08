b32a06193198254cef23e931e348306b64886791
Senthil Kumar S A <sasenthilk@gmail.com>
2021-10-08 07:34:42 +0530

    Modified: To accept valid address of contact

--- Address.js
+++ Address.js
@@ -14,13 +14,23 @@ class Address {
         return this.city;
     }
     set city(city) {
-        this._city = city;
+
+        let cityRegex = RegExp('^[A-Za-z]{4,}$')
+        if (cityRegex.test(city))
+            this._city = city;
+        else throw 'City is incorrect';
     }
     set state(state) {
-        this._state = state;
+        let stateRegex = RegExp('^[A-Za-z]{4,}$')
+        if (stateRegex.test(state))
+            this._state = state;
+        else throw 'State is incorrect';
     }
     set zip(zip) {
-        this._zip = zip;
+        let zipRegex = RegExp('^[0-9]{3}[]?[0-9]{3}$')
+        if (zipRegex.test(zip))
+            this._zip = zip;
+        else throw 'Zip is incorrect';
     }
 
     constructor(city, state, zip) {
@@ -30,4 +40,5 @@ class Address {
         this.zip = zip;
     }
 }
+
 module.exports = Address;
