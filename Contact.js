9b1f66c9094a677d1df665842e18f24bf6ee45c3
Senthil Kumar S A <sasenthilk@gmail.com>
2021-10-08 07:35:06 +0530

    Modified: To store valid contact details

--- Contact.js
+++ Contact.js
@@ -19,33 +19,41 @@ class Contact {
     get firstName() {
         return this._firstName;
     }
-
     set firstName(firstName) {
-        this._firstName = firstName;
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$')
+        if (nameRegex.test(firstName))
+            this._firstName = firstName;
+        else throw 'firstName is incorrect';
     }
 
     get lastName() {
         return this._lastName;
     }
-
     set lastName(lastName) {
-        this._lastName = lastName;
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$')
+        if (nameRegex.test(lastName))
+            this._lastName = lastName;
+        else throw 'lastName is incorrect';
     }
 
     get phoneNumber() {
         return this._phoneNumber;
     }
-
     set phoneNumber(phoneNumber) {
-        this._phoneNumber = phoneNumber;
+        let phoneNumberRegex = RegExp('^[0-9]{10}$')
+        if (phoneNumberRegex.test(phoneNumber))
+            this._phoneNumber = phoneNumber;
+        else throw 'phoneNumber is incorrect';
     }
 
     get email() {
         return this._email;
     }
-
     set email(email) {
-        this._email = email;
+        let emailRegex = RegExp('^((((abc)[a-zA-Z0-9]*)||(abc[_\\+\\-\\.][a-zA-Z0-9]*))+(?<!\\.)@([0-9a-zA-Z]{1,}))\\.(([a-zA-Z]{2,}\\.[a-zA-Z]{2})||([a-zA-Z\\,]{2,}))+(?<!\\.)$')
+        if (emailRegex.test(email))
+            this._email = email;
+        else throw 'email is incorrect';
     }
 
     toString() {
@@ -56,12 +64,17 @@ class Contact {
     }
 }
 
-let contact1 = new Contact('Test1', 'User1', 'Test City1', 'Test State1', 560001, '1111111111', 'test1@gmail.com');
-let contact2 = new Contact('Test2', 'User2', 'Test City2', 'Test State2', 660002, '2222222222', 'test2@gmail.com');
-let contact3 = new Contact('Test3', 'User3', 'Test City3', 'Test State2', 760002, '3333333333', 'test3@gmail.com');
+try {
 
-console.log(""+contact1)
-console.log(""+contact2)
-console.log(""+contact3)
+    let contact1 = new Contact('Test1', 'User1', 'Test City1', 'Test State1', 560001, '1111111111', 'test1@gmail.com');
+    let contact2 = new Contact('Test2', 'User2', 'Test City2', 'Test State2', 660002, '2222222222', 'test2@gmail.com');
+    let contact3 = new Contact('Test3', 'User3', 'Test City3', 'Test State2', 760002, '3333333333', 'test3@gmail.com');
+    console.log("" + contact1)
+    console.log("" + contact2)
+    console.log("" + contact3)
+}
+catch (exception) {
+    console.log(exception)
+}
 
 module.exports = Contact;
