56cab07bd7c10362c691c3e49d7a114e69e004b9
Senthil Kumar S A <sasenthilk@gmail.com>
2021-10-08 07:39:40 +0530

    Added: To store multiple contacts

---
+++ AddressBook.js
@@ -0,0 +1,13 @@
+class AddressBook {
+
+    contacts;
+
+    get contacts() {
+        return this.contacts;
+    }
+
+    set contacts(value) {
+        this.contacts = value;
+    }
+}
+module.exports = AddressBook;
